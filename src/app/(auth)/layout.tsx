49d3239954cbea1417d478e5f1084498641821b5
Cristiane Martins <crisplusm@gmail.com>
2025-02-20 19:47:42 -0300

    Grupos e rotas dinâmicas, installation of the codemode next-async-request-api to correct terminal api-async warnings

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function AuthLayout ({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            <h1 className="text-xl font-bold">Logo do App</h1>
+            <div>{children}</div>
+            
+        </div>
+    )
+}
