f586a617564cf247ea498db625f6985ab1bfc579
Cristiane Martins <crisplusm@gmail.com>
2025-02-20 18:53:46 -0300

    Fundamentals, Routes and Layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>Cabeçalho</div>
         {children}
       </body>
     </html>
