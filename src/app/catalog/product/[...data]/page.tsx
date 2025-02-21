be92f8a6e199b554093ccd99fa850645d8117e1d
Cristiane Martins <crisplusm@gmail.com>
2025-02-21 16:33:48 -0300

    React Server Components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 interface ProductProps {
   params: {
     data: string[]
@@ -7,12 +8,18 @@ interface ProductProps {
 export default function Product({params}: ProductProps) {
   
   const [productId, size, color] = params.data
+
+  function addToCart() {
+    console.log('Adcionou ao carrinho')
+  }
   
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adcionar ao carrinho</button>
     </div>
   );
 }
